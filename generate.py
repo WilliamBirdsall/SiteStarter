58042ceed6fbd41a085b62af4a660c06f44071a0
William Birdsall <wbirds1@students.towson.edu>
2018-11-09 15:10:42 -0500

    You can now call site-starter from the directory you want the new project folder to go into

--- generate.py
+++ generate.py
@@ -1,25 +1,28 @@
 import os
 import pathlib
 import shutil
+import sys
 
 
-def create_sub_dirs(project_name):
+def create_sub_dirs(project_dir):
     sub_dirs = ["css", "js", "imgs"]
-    for sub_dir_name in sub_dirs:
-        os.mkdir(project_name + "/" + sub_dir_name)
+    for sub_dir in sub_dirs:
+        sub_path = pathlib.PurePath(project_dir, sub_dir)
+        os.mkdir(sub_path)
 
-def generate_files(project_name):
+def generate_files(project_dir, site_starter_path):
     def create_index_html_file():
-        index_file_path = pathlib.PurePath(project_name, "index.html")
-        shutil.copyfile("index_template.html", index_file_path)
+        index_template_path = pathlib.PurePath(site_starter_path,"index_template.html")
+        index_file_path = pathlib.PurePath(project_dir, "index.html")
+        shutil.copyfile(index_template_path, index_file_path)
 
     def create_css_files():
-        css_file_path = pathlib.PurePath(project_name, "css", "styles.css")
+        css_file_path = pathlib.PurePath(project_dir, "css", "styles.css")
         f = open(css_file_path, "w")
         f.close()
 
     def create_js_file():
-        js_file_path = pathlib.PurePath(project_name, "js", "main.js")
+        js_file_path = pathlib.PurePath(project_dir, "js", "main.js")
         f = open(js_file_path, "w")
         f.close()
 
@@ -27,9 +30,17 @@ def generate_files(project_name):
     create_css_files()
     create_js_file()
 
+def main():
+    if len(sys.argv) <= 1:
+        print("Site Starter generates a project file with the specified project name in the cwd.")
+        print("Usage: python3 path_to_generate.py <project_name>")
+    else:
+        cwd = os.getcwd()
+        site_starter_path = pathlib.PurePath(sys.argv[0]).parent
+        project_dir = pathlib.PurePath(cwd, sys.argv[1])
+        os.mkdir(project_dir)
+        create_sub_dirs(project_dir)
+        generate_files(project_dir, site_starter_path)
 
-if __name__ == '__main__':
-    project_name = input("Enter project name: ")
-    os.mkdir(project_name)
-    create_sub_dirs(project_name)
-    generate_files(project_name)
+
+if __name__ == '__main__': main()
